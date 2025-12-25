f24409dc7a5b6d696beb117dc43e2d86e870e3da
tanzi <1107360+qqblack@user.noreply.gitee.com>
2025-12-25 22:31:21 +0100

    change the code into pipeline style

--- tools/mdtitle.py
+++ tools/mdtitle.py
@@ -1,50 +1,66 @@
 import os
 import re
 import sys
+from functools import reduce  # pipeline
 
 os.chdir(sys.path[0])
 
 input_path = "../output/ResidenceLawOrigin-remove.md"
 out_path = "../output/ResidenceLaw.md"
 
-with open(input_path, "r", encoding="utf-8") as f:
-    input_text = f.read()
+def chapter(input_text:str):
+    # Chapter 
+    pattern = re.compile(
+        r'\nChapter\s+(\d+)\s*\n\s*(.+)',
+        re.IGNORECASE
+    )
 
+    output_text = pattern.sub(r'\n## Chapter \1 \2', input_text)
+    return output_text
 
-# Chapter 
-pattern = re.compile(
-    r'\nChapter\s+(\d+)\s*\n\s*(.+)',
-    re.IGNORECASE
-)
+def division(input_text:str):
+    # Division
+    pattern = re.compile(
+        r'\Division\s+(\d+)\s*\n\s*(.+)',
+        re.IGNORECASE
+    )
 
-input_text1 = pattern.sub(r'\n## Chapter \1 \2', input_text)
+    output_text = pattern.sub(r'\n### Division \1 \2', input_text)
+    return output_text
 
-# Division
-pattern = re.compile(
-    r'\Division\s+(\d+)\s*\n\s*(.+)',
-    re.IGNORECASE
-)
+def section(input_text:str):
+    # Section
+    pattern = re.compile(
+        r'\nSection\s+(\d+)\s*\n\s*(.+)',
+        re.IGNORECASE
+    )
 
-input_text2 = pattern.sub(r'\n### Division \1 \2', input_text1)
+    output_text = pattern.sub(r'\n#### Section \1 \2', input_text)
+    return output_text
 
-# Section
-pattern = re.compile(
-    r'\nSection\s+(\d+)\s*\n\s*(.+)',
-    re.IGNORECASE
-)
+def sub_section(input_text:str):
+    # Section 1[abc]
+    pattern = re.compile(
+        r'\nSection\s+(\d+[a-z]?)\s*\n\s*(.+)',
+        re.IGNORECASE
+    )
 
-input_text3 = pattern.sub(r'\n#### Section \1 \2', input_text2)
+    output_text = pattern.sub(r'\n##### Section \1 \2', input_text)
+    return output_text
 
-# Section 1[abc]
-pattern = re.compile(
-    r'\nSection\s+(\d+[a-z]?)\s*\n\s*(.+)',
-    re.IGNORECASE
-)
+def apply_pipeline(input_text:str):
+    return reduce(lambda x, fun: fun(x), pipeline, input_text)
 
-output_text = pattern.sub(r'\n##### Section \1 \2', input_text3)
+if __name__ == "__main__":
+    pipeline = [chapter, division, section, sub_section]
 
-with open(out_path, "w", encoding="utf-8") as f:
-    f.write(output_text)
+    with open(input_path, "r", encoding="utf-8") as f:
+        input_text = f.read()
 
-print("done!")
+    output_text = apply_pipeline(input_text)
+
+    with open(out_path, "w", encoding="utf-8") as f:
+        f.write(output_text)
+
+    print("done!")
 
